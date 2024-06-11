f74f777d1f9d76cbc5156f0555a0cf8b5bcb0c7d
AndreaParrini <andrea.parrini26@gmail.com>
2024-06-11 18:46:05 +0200

    create relation to model category and foto

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'slug'];
+
+    /**
+     * Get all of the fotos for the Category
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function fotos(): HasMany
+    {
+        return $this->hasMany(Foto::class);
+    }
 }
--- app/Models/Foto.php
+++ app/Models/Foto.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Foto extends Model
 {
     use HasFactory;
 
     protected $fillable = ['title', 'description', 'slug', 'image_path', 'in_evidenza'];
+
+    /**
+     * Get the category that owns the Foto
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
